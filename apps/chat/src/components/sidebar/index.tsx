2ff0cddf13f1f3819ca6e54e59cace38d8de0877
mfxiaosheng <1211222334@qq.com>
2023-05-06 14:29:19 +0800

    去除公众号

--- apps/chat/src/components/sidebar/index.tsx
+++ apps/chat/src/components/sidebar/index.tsx
@@ -85,7 +85,7 @@ export function Sidebar({ children }: { children: React.ReactNode }) {
           <div className={styles["sidebar-title"]}>{Locale.Index.Title}</div>
           <div className={styles["sidebar-sub-title"]}>
             {Locale.Index.SubTitle}{" "}
-            <span className={styles["sidebar-ad"]}>Magic万事屋</span>
+            <span className={styles["sidebar-ad"]}></span>
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
