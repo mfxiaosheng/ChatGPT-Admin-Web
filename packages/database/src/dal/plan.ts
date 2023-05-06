114ab6cd5b97b5794fe05b70da4e704b3c0f81e6
mfxiaosheng <1211222334@qq.com>
2023-05-06 14:14:42 +0800

    Revert "Update plan.ts"
    This reverts commit 0ab682ffe13b503fcf3d909d64fc98f83b6b9472.

--- packages/database/src/dal/plan.ts
+++ packages/database/src/dal/plan.ts
@@ -5,52 +5,6 @@ export class PlanDAL extends AbstractDataAccessLayer<Plan> {
   schema = plan;
   namespace: `${string}:` = "plan:";
 
-  constructor() {
-    super();
-    this.initializePlans();
-  }
-
-  async initializePlans() {
-    // Check if the plans already exist
-    const basicPlanExists = await this.redis.exists(this.getKey('Pro'));
-    const premiumPlanExists = await this.redis.exists(this.getKey('premium'));
-
-    // If the plans do not exist, create them
-    if (!basicPlanExists) {
-      const basicPlan: Plan = {
-        prices: {
-          monthly: 30,
-          quarterly: 79,
-          yearly: 259,
-        },
-        limits: {
-          window: {
-            limit: 50,
-            window: '3h',
-          },
-        },
-      };
-      await this.redis.json.set(this.getKey('Pro'), '$', basicPlan);
-    }
-
-    if (!premiumPlanExists) {
-      const premiumPlan: Plan = {
-        prices: {
-          monthly: 129,
-          quarterly: 326,
-          yearly: 999,
-        },
-        limits: {
-          window: {
-            limit: 50,
-            window: '1h',
-          },
-        },
-      };
-      await this.redis.json.set(this.getKey('premium'), '$', premiumPlan);
-    }
-  }
-
   async doCreate(name: string, data: Plan) {
     await this.redis.json.set(this.getKey(name), "$", data);
   }
